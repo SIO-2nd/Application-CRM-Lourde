ef2a230d2ba02504fd410856e5959e75169f3b07
$traker <38327800+Str4ky@users.noreply.github.com>
2021-09-15 13:17:25 +0200

    Add files via upload

---
+++ Produits.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Lourde_CRM
+{
+    class Produits
+    {
+
+        #region Champs
+        private int _numProduit;
+        private string _nomProduit;
+        private string _typeProduit;
+        private int _refProduit;
+        #endregion
+
+        #region Constructeurs
+        public Produits(int num, string nom, string type, int reference)
+        {
+            _numProduit = num;
+            _nomProduit = nom;
+            _typeProduit = type;
+            _refProduit = reference;
+
+        }
+        #endregion
+
+        #region Accesseurs/Mutateurs
+        public int NumProduit
+        {
+            get { return _numProduit; }
+            set { _numProduit = value; }
+        }
+
+        public string NomProduit
+        {
+            get { return _nomProduit; }
+            set { _nomProduit = value; }
+        }
+
+        public string TypeProduit
+        {
+            get { return _typeProduit; }
+            set { _typeProduit = value; }
+        }
+        public int RefProduit
+        {
+            get { return _refProduit; }
+            set { _refProduit = value; }
+        }
+        #endregion
+
+        #region Methodes
+        public override string ToString()
+        {
+            return Convert.ToString(_numProduit);
+        }
+        #endregion
+    }
+}
---
+++ Prospects.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Lourde_CRM
+{
+    class Prospects
+    {
+
+        #region Champs
+        private int _numProspect;
+        private string _nomProspect;
+        private string _prenomProspect;
+        private string _mailProspect;
+        private int _telProspect;
+        private string _adresseProspect;
+        private string _villeProspect;
+        private int _cpProspect;
+        #endregion
+
+        #region Constructeurs
+        public Prospects(int num, string nom, string prenom, string mail, int tel, string adresse, string ville, int cp)
+        {
+            _numProspect = num;
+            _nomProspect = nom;
+            _prenomProspect = prenom;
+            _mailProspect = mail;
+            _telProspect = tel;
+            _adresseProspect = adresse;
+            _villeProspect = ville;
+            _cpProspect = cp;
+        }
+        #endregion
+
+        #region Accesseurs/Mutateurs
+        public int NumProspect
+        {
+            get { return _numProspect; }
+            set { _numProspect = value; }
+        }
+
+        public string NomProspect
+        {
+            get { return _nomProspect; }
+            set { _nomProspect = value; }
+        }
+        public string PrenomProspect
+        {
+            get { return _prenomProspect; }
+            set { _prenomProspect = value; }
+        }
+
+        public string MailProspect
+        {
+            get { return _mailProspect; }
+            set { _mailProspect = value; }
+        }
+
+        public int TelProspect
+        {
+            get { return _telProspect; }
+            set { _telProspect = value; }
+        }
+        public string AdresseProspect
+        {
+            get { return _adresseProspect; }
+            set { _adresseProspect = value; }
+        }
+        public string VilleProspect
+        {
+            get { return _villeProspect; }
+            set { _villeProspect = value; }
+        }
+
+        public int CpProspect
+        {
+            get { return _cpProspect; }
+            set { _cpProspect = value; }
+        }
+        #endregion
+
+        #region Methodes
+        public override string ToString()
+        {
+            return Convert.ToString(_numProspect);
+        }
+        #endregion
+    }
+}
