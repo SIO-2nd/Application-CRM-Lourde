9e928e6aab814239e539643ca84ea75cd7380ad9
Goupil117 <84926681+Goupil117@users.noreply.github.com>
2021-09-29 16:51:25 +0200

    Add files via upload

--- BDD.cs
+++ BDD.cs
@@ -100,17 +100,38 @@ namespace Application_Lourde_CRM
             }
         }
 
-        public bool Ajouter()
+        #region Ajouter
+
+        public bool Ajouter_Prospects(Prospects prospects)
         {
             if(this.Ouverture_Connexion())
+            // Ouverture de la connexion SQL + vérification
             {
+
+
+                // Création d'une commande SQL en fonction de l'objet connexion
                 MySqlCommand requete = this.connexion.CreateCommand();
 
-                requete.CommandText = "INSERT INTO" + " " +
+                // Requête SQL
+                requete.CommandText = "INSERT INTO prospects (IdPro, NomPro, PrePro, AdrPro, CpPro, VillePro, MailPro, TelPro) VALUES(@idPro, @NomPro, @PrePro, @AdrPro, @CpPro, @VillePro, @MailPro, @TelPro)";
 
+                // Utilisation de l'objet Prospects passé en paramètre
+                requete.Parameters.AddWithValue("@IdPro", prospects.NumProspect);
+                requete.Parameters.AddWithValue("@NomPro", prospects.NomProspect);
+                requete.Parameters.AddWithValue("@PrePro", prospects.PrenomProspect);
+                requete.Parameters.AddWithValue("@AdrPro", prospects.AdresseProspect);
+                requete.Parameters.AddWithValue("@CpPro", prospects.CpProspect);
+                requete.Parameters.AddWithValue("@VillePro", prospects.VilleProspect);
+                requete.Parameters.AddWithValue("@MailPro", prospects.MailProspect);
+                requete.Parameters.AddWithValue("@TelPro", prospects.TelProspect);
+
+                //Exécution de la commande SQL
                 requete.ExecuteNonQuery();
 
+
+                // Fermeture de la connexion
                 this.Fermeture_Connexion();
+
                 return true;
             }
             else
@@ -120,20 +141,160 @@ namespace Application_Lourde_CRM
             }
         }
 
-        public bool Modifier()
+        public bool Ajouter_Client(Client client)
         {
-            if(this.Ouverture_Connexion())
+            if (this.Ouverture_Connexion())
+            // Ouverture de la connexion SQL + vérification
             {
-                this.Ouverture_Connexion();
+
+
+                // Création d'une commande SQL en fonction de l'objet connexion
+                MySqlCommand requete = this.connexion.CreateCommand();
+
+                // Requête SQL
+                requete.CommandText = "INSERT INTO clients (IdCli, NomCli, PreCli, AdrCli, CpCli, VilleCli, MailCli, TelCli) VALUES(@IdCli, @NomCli, @PreCli, @AdrCli, @CpCli, @VilleCli, @MailCli, @TelCli)";
+
+                // Utilisation de l'objet Prospects passé en paramètre
+                requete.Parameters.AddWithValue("@IdCli", client.ID_Client);
+                requete.Parameters.AddWithValue("@NomCli", client.Nom_Client);
+                requete.Parameters.AddWithValue("@PreCli", client.Prenom_Client);
+                requete.Parameters.AddWithValue("@AdrCli", client.Adresse_Client);
+                requete.Parameters.AddWithValue("@CpCli", client.Code_Postal_Client);
+                requete.Parameters.AddWithValue("@VilleCli", client.Ville_Client);
+                requete.Parameters.AddWithValue("@MailCli", client.Email_Client);
+                requete.Parameters.AddWithValue("@TelCli", client.Numero_Client);
+
+                //Exécution de la commande SQL
+                requete.ExecuteNonQuery();
+
+
+                // Fermeture de la connexion
+                this.Fermeture_Connexion();
+
                 return true;
             }
-            catch
+            else
             {
+                Console.WriteLine("Erreur");
                 return false;
             }
         }
 
-        public bool
+        public bool Ajouter_Commercials(Commercials commercials)
+        {
+            if (this.Ouverture_Connexion())
+            // Ouverture de la connexion SQL + vérification
+            {
+
+
+                // Création d'une commande SQL en fonction de l'objet connexion
+                MySqlCommand requete = this.connexion.CreateCommand();
+
+                // Requête SQL
+                requete.CommandText = "INSERT INTO commercials (idCommercial, NomCommercial, PreCommercial, TelCommercial, MailCommercial) VALUES(@idCommercial, @NomCommercial, @PreCommercial, @TelCommercial, @MailCommercial)";
+
+                // Utilisation de l'objet Prospects passé en paramètre
+                requete.Parameters.AddWithValue("@IdCommercial", commercials.ID_Commercials);
+                requete.Parameters.AddWithValue("@NomCommercial", commercials.Nom_Commercials);
+                requete.Parameters.AddWithValue("@PreCommercial", commercials.Prenom_Commercials);
+                requete.Parameters.AddWithValue("@TelCommercial", commercials.Numero_Commercials);
+                requete.Parameters.AddWithValue("@MailCommercial", commercials.Email_Commercials);
+
+                //Exécution de la commande SQL
+                requete.ExecuteNonQuery();
+
+
+                // Fermeture de la connexion
+                this.Fermeture_Connexion();
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Erreur");
+                return false;
+            }
+        }
+
+        public bool Ajouter_Produits(Produits produits)
+        {
+            if (this.Ouverture_Connexion())
+            // Ouverture de la connexion SQL + vérification
+            {
+
+
+                // Création d'une commande SQL en fonction de l'objet connexion
+                MySqlCommand requete = this.connexion.CreateCommand();
+
+                // Requête SQL
+                requete.CommandText = "INSERT INTO produit (IdProd, TypeProd, PrixProd, NomProd, LibProd) VALUES(@IdProd, @TypeProd, @PrixProd, @NomProd, @LibProd)";
+
+                // Utilisation de l'objet Prospects passé en paramètre
+                requete.Parameters.AddWithValue("@IdProd", produits.ID);
+                requete.Parameters.AddWithValue("@TypeProd", produits.TypeProduit);
+                requete.Parameters.AddWithValue("@PrixProd", produits.PrixProduit);
+                requete.Parameters.AddWithValue("@NomProd", produits.NomProduit);
+                requete.Parameters.AddWithValue("@LibProd", produits.RefProduit);
+
+                //Exécution de la commande SQL
+                requete.ExecuteNonQuery();
+
+
+                // Fermeture de la connexion
+                this.Fermeture_Connexion();
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Erreur");
+                return false;
+            }
+        }
+
+        public bool Facture(Facture facture)
+        {
+            if (this.Ouverture_Connexion())
+            // Ouverture de la connexion SQL + vérification
+            {
+
+
+                // Création d'une commande SQL en fonction de l'objet connexion
+                MySqlCommand requete = this.connexion.CreateCommand();
+
+                // Requête SQL
+                requete.CommandText = "INSERT INTO produit (IdProd, TypeProd, PrixProd, NomProd, LibProd) VALUES(@IdProd, @TypeProd, @PrixProd, @NomProd, @LibProd)";
+
+                // Utilisation de l'objet Prospects passé en paramètre
+                requete.Parameters.AddWithValue("@IdProd", facture.ID_Facture);
+                requete.Parameters.AddWithValue("@TypeProd", facture.Date_Facture);
+                requete.Parameters.AddWithValue("@PrixProd", facture.I);
+                requete.Parameters.AddWithValue("@NomProd", .);
+                requete.Parameters.AddWithValue("@LibProd", .);
+
+                //Exécution de la commande SQL
+                requete.ExecuteNonQuery();
+
+
+                // Fermeture de la connexion
+                this.Fermeture_Connexion();
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Erreur");
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Modifier
+
+  
+        #endregion
+
         #endregion
     }
 }
